30243937b96b8ef18e200c1213686c06bafe92a5
unknown <christinesoto21@gmail.com>
2020-04-20 12:18:33 -0500

    Superhero model created

---
+++ SuperHeroCreator/Models/SuperHero.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroCreator.Models
+{
+    public class SuperHero
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name="Superhero Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Alter Ego Name")]
+        public string AlterEgoName { get; set; }
+
+        [Display(Name = "Primary Superhero Ability")]
+        public string PrimaryAbility { get; set; }
+
+        [Display(Name = "Secondary Superhero Ability")]
+        public string SecondaryAbility { get; set; }
+
+        public string Catchphrase { get; set; }
+    }
+}
